560822f67a8a518c7d808e4b62c25aeb4f12f1f3
Kapil Kumar <kapil.kumar108kk@gmail.com>
2025-07-17 17:23:15 +0530

    Episode 02

--- Episode 02 - Igniting our App/App.js
+++ Episode 02 - Igniting our App/App.js
@@ -1,48 +1,8 @@
-// const heading = React.createElement("h1", { id: "heading" }, "Jai Shree Ram");
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-
-
-{
-    /* <div id="parent">
-      <div id="child">
-          <h1>Child 1</h1>
-          <h1>Child 2</h1>
-      </div>
-  </div> */
-}
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement("div", { id: "child" }, [
-//     React.createElement("h1", {key:'1'}, "Child 1"),
-//     React.createElement("h1", {key:'2'}, "Child 1"),
-//   ])
-// );
-
-// ReactDOM.createRoot(document.getElementById('root')).render(parent)
-
-
-
-
-/* <div id="parent">
-  <div id="child 1">
-      <h1>Child 1</h1>
-      <h1>Child 2</h1>
-  </div>
-  <div id="child 2">
-      <h1>Child 1</h1>
-      <h1>Child 2</h1>
-  </div>
-</div> */
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 const parent = React.createElement(
     "div",
-    { id: "parent" },
+    { id: "parent" }, 
     [
         React.createElement("div", { id: "child1", key:'1'}, [
             React.createElement("h1", { key: '1' }, "Child 1"),
